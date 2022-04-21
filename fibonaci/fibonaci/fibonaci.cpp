a71f86f07923173f3abead87fce698b1fd5a7420
Gruzin387 <vhruzin387@technikumkreatywne.pl>
2022-04-21 11:07:28 +0200

    nic

---
+++ fibonaci/fibonaci/fibonaci.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int f0 = 0, f1 = 1, f;
+
+    cout << "Podaj ile chcesz wypisać wyrazów ciągu fibonacciego: ";
+    cin >> n;
+    for (int i = 0;i < n;i++) {
+        if (i > 1) {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+            
+        }
+        else{
+            f = i;
+        }
+        cout << f << " ";
+    }
+   
+
+    return 0;
+}
